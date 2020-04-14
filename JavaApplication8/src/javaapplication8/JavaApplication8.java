d92dafd2b449c3b204374ec6cdd86647467f7161
Carlos144Green <60107217+Carlos144Green@users.noreply.github.com>
2020-04-14 18:34:58 -0500

    Merge origin/master
    Conflicts:
    	src/workoutuno/WorkoutUno.java

--- JavaApplication8/src/javaapplication8/JavaApplication8.java
+++ JavaApplication8/src/javaapplication8/JavaApplication8.java
@@ -1,23 +1,98 @@
-//test test to see if edit orked
-
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package javaapplication8;
+package bang_dice_game;
 
 /**
  *
- * @author lolc4
+ * @author airishimamura
  */
-public class JavaApplication8 {
+import java.lang.Math;
+import java.util.Random; 
+import java.util.Scanner;
 
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        // TODO code application logic here
+public class Dice {  
+    enum Dice_Face
+                 
+    { 
+        IndianArrow, Dynamite, BullsEye1, BullsEye2, Beer, Gatling;
+        
+        private static final Dice_Face [] dice = Dice_Face.values();
+        private static final int SIZE = dice.length;
+        private static final Random RANDOM = new Random((long)( Math.random() * (double)Long.SIZE));
+        
+        public static Dice_Face getRandomDice_Face(){      
+            return dice[RANDOM.nextInt(SIZE)];
+
+        }
+     
+    }  
+      public static void firstRoll() //method to roll 6 dice
+      {
+        for ( int i = 0; i < Dice_Face.SIZE; i++){
+            Dice_Face.dice [i] = Dice_Face.getRandomDice_Face();
+            System.out.print("\t" + "Dice[" + i + "] = " + Dice_Face.dice[i] + "\n " );
+        }
+      }
+       
+    private final Dice_Face dice_face;
+    
+    //setter 
+    public Dice(Dice_Face dice_face)
+    {
+        this.dice_face = dice_face;
     }
     
+    //getter 
+   public Dice_Face getDice_Face () 
+   {  
+    return this.dice_face;
+    
+    }
+   
+   public static void reRoll (){
+       
+       Scanner scanner = new Scanner(System.in);
+       System.out.println("\nChose number of dice you want to reroll: ");
+       int num = scanner.nextInt();  
+            
+       if (0 <= num && num < 6){
+           //when the dice was dynamite, print "you cannot reroll"
+          if(Dice_Face.dice [num] == Dice.Dice_Face.Dynamite){
+              System.out.println("You cannot reroll\n");
+          }
+          else{
+             System.out.println("Dice Face after reroll:\n");
+              for ( int j = 0; j < Dice_Face.SIZE; j++ ){
+                  //get new dice face 
+                if(j == num){
+                 Dice_Face.dice[j] = Dice_Face.getRandomDice_Face();
+                   }
+             //displaying the dice after re-roll
+            System.out.print("\t" + "Dice[" + j + "] = " + Dice_Face.dice[j] + "\n "); 
+              }
+          }
+       }
+       //when the given number is invalid dice number 
+      else{
+            System.out.println("Invalid Dice number \n");
+       }   
+   }
+   
+   
+//   //just for testing
+//   public static void main(String[] args) {
+//
+//    Dice roll = new Dice(Dice_Face.getRandomDice_Face());
+//    roll.firstRoll();
+//    
+//    for(int z = 0; z < 3; z++){
+//    roll.reRoll();
+//    }
+//
+//    
+//    }  
+   
 }
