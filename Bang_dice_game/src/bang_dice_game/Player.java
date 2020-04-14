9255426f7dc768e4cb5016b4d74a921f2f823c34
ams171615 <55107914+ams171615@users.noreply.github.com>
2020-04-14 18:58:04 -0500

    Add files via upload

---
+++ Bang_dice_game/src/bang_dice_game/Player.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bang;
+
+/**
+ *
+ * @author sloan
+ */
+public interface Player {
+    
+    String name = "";
+    String description = "";
+    String role = "";
+    int health = 0;
+    int arrows = 0;
+    
+    
+    public int getHealth();
+    public void setHealth(int health);
+    
+    public String getRole();
+    public void setRole(String role);
+    
+    public String getDescription();
+    public void setDecription(String description);
+    
+    public int getArrow();
+    public void setArrow(int arrows);
+    
+}
