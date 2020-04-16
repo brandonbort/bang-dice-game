b75d79bb97d2a6fb727ea7c03d41287d77a02556
Brandon <Brandon@DESKTOP-VK7TPOH>
2020-04-16 17:26:58 -0500

    Merging changes from different directories

---
+++ BangDiceGame/src/bang_dice_game/Player.java
@@ -0,0 +1,34 @@
+  
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bang_dice_game;
+
+/**
+ *
+ * @author sloan
+ */
+public interface Player {
+    
+    /*String name = "";
+    String description = "";
+    String role = "";
+    int health;
+    int arrows;*/
+    
+    
+    public int getHealth();
+    public void setHealth(int health);
+    
+    public String getRole();
+    public void setRole(String role);
+    
+    public String getDescription();
+    public void setDecription(String descrip);
+    
+    public int getArrows();
+    public void setArrows(int arrows);
+    
+}
