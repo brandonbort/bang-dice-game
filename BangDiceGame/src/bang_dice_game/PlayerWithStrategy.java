c01c895c1fff556848d99a15fb10e385b25b5ae9
carlos144green <carlos144green@DESKTOP-DB3O50U>
2020-04-30 12:55:21 -0500

    OwO

--- BangDiceGame/src/bang_dice_game/PlayerWithStrategy.java
+++ BangDiceGame/src/bang_dice_game/PlayerWithStrategy.java
@@ -9,46 +9,63 @@ package bang_dice_game;
  *
  * @author carlos144green
  */
+
+//I have no idea what im doing so edit to ur content
 class PlayerWithStrategy {
 
-    private int getHealth;
-    private String getRole;
-    private String getDescription;
-    private int getArrows;
+    private String name = "";
+    private String description = "";
+    private String role = "";
+    private int health = 0;
+    private int arrows = 0;
     private PlayStrategy strategy;
   
   /**
-   * Create a new Player with the specified name and strategy.
+   * Create a NPC with the specified name, role, description, health, arrows, and strategy.
    */
-  public PlayerWithStrategy (String getDescription, PlayStrategy strategy) {
-    this.name = name;
-    this.strategy = strategy;
-    this.sticksTaken = 0;
-  }
+  public PlayerWithStrategy(String name, String role, String description, int health, int arrows, PlayStrategy strategy){
+        this.name = name;
+        this.role = role;
+        this.description = description;
+        this.health = health;
+        this.arrows = arrows;       //arrows should always start at 0 but ill include it here anyways
+        this.strategy= strategy;    //assign a strategy to the NPC
+    }
   
-  /**
-   * This Player's name.
-   */
-  public String name () {
-    return this.name;
-  }
-  
-  /**
-   * Number of sticks removed on this Player's most
-   * recent turn. Returns 0 if this Player has not yet
-   * taken a turn.
-   * @ensure     this.sticksTaken() >= 0
-   */
-  public int sticksTaken () {
-    return this.sticksTaken();
-  }
-  
-  /**
-   * Set the play strategy for this Player.
-   */
-  public void setStrategy (PlayStrategy strategy) {
-    this.strategy = strategy;
-  }
+    public int getHealth() {
+        return health;
+    }
+    public void setHealth(int health) {
+        this.health = health;
+    }
+    
+    public String getRole() {
+        return this.role;
+    }
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+    public void setDecription(String descrip) {
+        this.description = descrip;
+    }
+
+    public int getArrows() {
+        return this.arrows;
+    }
+    public void setArrows(int arrows) { 
+        this.arrows = arrows;
+    }
+    
+    public PlayStrategy getStrategy() {
+        return this.strategy;
+    }
+    public void setStrategy(PlayStrategy strategy) { 
+        this.strategy = strategy;
+    }
   
   /**
    * Take a turn: remove sticks from the specified
