0de770fad191eee44726d0ade7c0293c8576f086
carlos144green <carlos144green@DESKTOP-DB3O50U>
2020-04-30 11:10:00 -0500

    OwO

---
+++ BangDiceGame/src/bang_dice_game/PlayerWithStrategy.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package bang_dice_game;
+
+/**
+ *
+ * @author carlos144green
+ */
+class PlayerWithStrategy {
+    
+    
+
+    private int getHealth();
+    
+    private String getRole();
+    
+    private String getDescription();
+        
+    private int getArrows();
+
+    public PlayerWithStrategy ()
+    
+    public int getHealth() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void setHealth(int health) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public String getRole() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void setRole(String role) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public String getDescription() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void setDecription(String descrip) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public int getArrows() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void setArrows(int arrows) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public void takeTurn() {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+    
+}
