e6b7271ba82d233a1d0f494f2520032602d92258
carlos144green <carlos144green@DESKTOP-DB3O50U>
2020-04-30 11:14:54 -0500

    OwO

--- BangDiceGame/src/bang_dice_game/PlayerWithStrategy.java
+++ BangDiceGame/src/bang_dice_game/PlayerWithStrategy.java
@@ -10,61 +10,6 @@ package bang_dice_game;
  * @author carlos144green
  */
 class PlayerWithStrategy {
-    
-    
 
-    private int getHealth();
-    
-    private String getRole();
-    
-    private String getDescription();
-        
-    private int getArrows();
-
-    public PlayerWithStrategy ()
-    
-    public int getHealth() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public void setHealth(int health) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public String getRole() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public void setRole(String role) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public String getDescription() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public void setDecription(String descrip) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public int getArrows() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public void setArrows(int arrows) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
-
-    @Override
-    public void takeTurn() {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
     
 }
