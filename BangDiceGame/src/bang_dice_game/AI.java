4ddbd4c48cd1b587e7a4fd227f7c9d0f946491d1
carlos144green <carlos144green@DESKTOP-DB3O50U>
2020-04-30 20:01:52 -0500

    OwO

--- BangDiceGame/src/bang_dice_game/AI.java
+++ BangDiceGame/src/bang_dice_game/AI.java
@@ -15,21 +15,21 @@ import java.util.*;
 // value in each position will determine who the player shoots. the sheriff will
 // be at position 0 and all the players will fill in the array counter clockwise
 // in the array. So if the sheriff gets shot by a person 2 distance away to his
-// left from him the array will be as follows with 5 players
-// karma_points:    0, 0,-1, 0, 0
-//                  0, 0, 0, 0, 0
-//                  0, 0, 0, 0, 0
-//                  0, 0, 0, 0, 0
-//                  0, 0, 0, 0, 0
+// left from him the array will be as follows with 5 players. x=null
+// karma_points:    x, 0,-1, 0, 0
+//                  0, x, 0, 0, 0
+//                  0, 0, x, 0, 0
+//                  0, 0, 0, x, 0
+//                  0, 0, 0, 0, x
     
 //if the sheriff then gets shot by a person to his right but healed by a person
 // from his left and the person 2 distance away from his right dies and reveals
-// his roll it will look something like this. x=null
-// karma_points:    1, 0,-1, x,-1
-//                  0, 0, 0, 0, 0
-//                  0, 0, 0, 0, 0
+// his roll it will look something like this. 
+// karma_points:    x, 1,-1, x,-1
+//                  0, x, 0, x, 0
+//                  0, 0, x, x, 0
 //                  x, x, x, x, x
-//                  0, 0, 0, 0, 0
+//                  0, 0, 0, x, x
 //
 //Clearly this is only if the sheriff gets attacked and roles arent revealed yet
 // because once roles start to get revealed karma will be used more or less
@@ -38,35 +38,47 @@ import java.util.*;
 // with opinions about the sheriff because you want to kill or save him. Renagade
 // will be a special case because his opinion of the sheriff will change once the 
 // outlaws are dead.
-// karma_points:    0, 0, 0, 0, 0   <Sheriff
-//               -100, 0, 0, 0, 0   <Outlaw1
-//                100, 0, 0, 0, 0   <Renegade
-//               -100, 0, 0, 0, 0   <Outlaw2
-//                100, 0, 0, 0, 0   <Deputy
+// karma_points:    x, 0, 0, 0, 0   <Sheriff
+//               -100, x, 0, 0, 0   <Outlaw1
+//                100, 0, x, 0, 0   <Renegade
+//               -100, 0, 0, x, 0   <Outlaw2
+//                100, 0, 0, 0, x   <Deputy
     
 //Lets say the Sheriff shoots deputy accidentally, outlaw1 shoots sheriff, renegade
 // shoots outlaw to his right (outlaw1), outlaw2 shoots outlaw1, deputy heals
 // sheriff.
-// karma_points:    0,-1, 0, 0, 1   <Sheriff
-//               -100, 0,-1,-1, 0   <Outlaw1
-//                100, 0, 0, 0, 0   <Renegade
-//               -100, 0, 0, 0, 0   <Outlaw2
-//                 99, 0, 0, 0, 0   <Deputy
+// karma_points:    x,-1, 0, 0, 1   <Sheriff
+//               -100, x,-1,-1, 0   <Outlaw1
+//                100, 0, x, 0, 0   <Renegade
+//               -100, 0, 0, x, 0   <Outlaw2
+//                 99, 0, 0, 0, x   <Deputy
 
 //So now deputy has good karma in sheriff's eyes so when sheriff decides to shoot
 // he wont shoot anyone with good karma. Thank you for coming to my ted talk.
 public class AI {
-   public void SheriffTurn (int[][] karma) {
-        //shoot
+    public void SheriffTurn (int karma[][]) {
+        //shoot1
+        int r=0;//Sheriff position always
+        int target=karma[r][1];
         for (int i=0; i<karma.length; i++)
+        {
+         //see who is alive
+            if(karma[r][i]==null)
+                
+            
+            
+            
+            if(target>karma[r][i])
+                target=karma[r][i];                
+        }
     }
-   public void DeputyTurn () {
+    public void DeputyTurn () {
         
     }
-   public void OutLawTurn () {
+    public void OutLawTurn () {
         
     }
-   public void RenegadeTurn () {
+    public void RenegadeTurn () {
         
     }
 }
