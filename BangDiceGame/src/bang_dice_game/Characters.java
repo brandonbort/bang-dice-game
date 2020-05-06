966d3252aed0c691c80e54605798bc24e26a80eb
Brandon <Brandon@DESKTOP-VK7TPOH>
2020-05-06 00:07:09 -0500

    lots of changes

--- BangDiceGame/src/bang_dice_game/Characters.java
+++ BangDiceGame/src/bang_dice_game/Characters.java
@@ -17,24 +17,39 @@ import java.security.SecureRandom;
  */
 public class Characters{
 
-    static int playerSIZE = BangDiceGame.karma.length;
+//    static int playerSIZE = BangDiceGame.karma.length;
     private static final Random RANDOM = new SecureRandom();
     public ArrayList <String> players = new ArrayList<>();
     
     public enum BasicCharNames{
         BartCassidy, BlackJack, CalamityJanet, ElGringo, JesseJones,
         Jourdonnais, KitCarlson, LuckyDuke, PaulRegret, PedroRamirez,
-        RoseDoolan, SidKetchum, SlabTheKiller,SuzyLafayette, VurltureSam,
+        RoseDoolan, SidKetchum, SlabTheKiller,SuzyLafayette, VultureSam,
         WillyTheKid;
         public static BasicCharNames playerName[] = BasicCharNames.values();
-        public static BasicCharNames getRandomPlayers(){      
-            return playerName[RANDOM.nextInt(playerSIZE)];
-            }
+//        public static BasicCharNames getRandomPlayers(){      
+//            return playerName[RANDOM.nextInt(playerSIZE)];
+//            }
     }    
     
     public void characterDescriptions(){
         
         
-    }    
+    }  
+    
+    public static LinkedList<String> getRandChars(int num){
+        LinkedList<String> chars = new LinkedList<String>();
+        for(int i = 0; i < BasicCharNames.values().length; i++){
+            chars.add(BasicCharNames.playerName[i].toString());
+        }
+        Collections.shuffle(chars);
+        int x = BasicCharNames.values().length;
+        while(x > num){
+            chars.pop();
+            x--;
+        }
+        return chars;
+    }
+
     
 }
